5a6b142cb70f68d3777d54614c3f77f57f081c2f
cyishere <cyishere@gmail.com>
2021-03-24 13:22:03 +0800

    add styled-components with storybook

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,16 +1,38 @@
 import styled from "styled-components/macro";
-import { COLORS } from "../../utils/constants";
+import { COLORS } from "../../styles/constants";
 
-const Button = ({ children, ...rest }) => {
-  return <Wrapper {...rest}>{children}</Wrapper>;
+const VARIANTS = {
+  default: {
+    "--backgroundColor": COLORS.grayLightDim,
+    "--textColor": COLORS.text,
+  },
+  primary: {
+    "--backgroundColor": COLORS.primary,
+    "--textColor": COLORS.white,
+  },
+  secondary: {
+    "--backgroundColor": COLORS.secondary,
+    "--textColor": COLORS.white,
+  },
+};
+
+const Button = ({ variant, children, ...rest }) => {
+  const styles = VARIANTS[variant];
+
+  return (
+    <Wrapper style={styles} {...rest}>
+      {children}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.button`
-  background-color: ${COLORS.primary};
-  color: ${COLORS.white};
-  padding: 8px 16px;
+  background-color: var(--backgroundColor);
+  color: var(--textColor);
+  padding: 16px 24px;
   text-transform: uppercase;
   border: none;
+  font: inherit;
 `;
 
 export default Button;
--- frontend/src/components/Button/Button.stories.js
+++ frontend/src/components/Button/Button.stories.js
@@ -3,8 +3,24 @@ import { action } from "@storybook/addon-actions";
 import Button from "./Button";
 
 export default {
-  title: "Component/Button",
+  title: "Components/Button",
   component: Button,
 };
 
-export const Text = () => <Button onClick={action("Clicked")}>signup</Button>;
+export const Default = () => (
+  <Button variant="default" onClick={action("Clicked")}>
+    login
+  </Button>
+);
+
+export const Primary = () => (
+  <Button variant="primary" onClick={action("Clicked")}>
+    signup
+  </Button>
+);
+
+export const Secondary = () => (
+  <Button variant="secondary" onClick={action("Clicked")}>
+    add to cart
+  </Button>
+);
